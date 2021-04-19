c6db04b2f2f48c28366833c15701d8281408bed4
flor.stellamans <flor.stellamans@student.howest.be>
2021-04-19 23:07:08 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function currentTime(){
+
+    let date = new Date();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();  
+    
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+
+    var t = setTimeout(function(){ currentTime() }, 1000); 
+
+    let midnight = "PM";
+    midnight = (hour <= 12) ? "AM" : "PM"   
+
+    document.querySelector("#clock").innerHTML = hour + " : " + min + " : " + sec + " " + midnight;
+}
+
+function updateTime(k){
+    if (k < 10) {
+        return "0" + k;
+    } else {
+        return k;
+    }
+}
+
+currentTime();
